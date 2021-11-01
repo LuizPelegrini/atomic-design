1acde7dbd6328022795b5d4ccabe9f9b805d7070
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2021-11-01 22:11:57 +0800

    add: molecule NavBar created

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="component">
-    <Logo />
+    <NavBar />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { Logo } from '@/components/atoms';
+import { NavBar } from '@/components/molecules';
 
 export default defineComponent({
   components: {
-    Logo
+    NavBar
   }
 });
 </script>
---
+++ src/components/molecules/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="nav-bar">
+    <NavItem text="Services" to="/services" />
+    <NavItem text="About" to="/about" />
+    <NavItem text="Cases" to="/cases" />
+    <NavItem text="Contact" to="/contact" />
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { NavItem } from '@/components/atoms';
+
+export default defineComponent({
+  name: 'NavBar',
+
+  components: { NavItem }
+});
+</script>
+
+<style lang="scss" scoped>
+.nav-bar {
+  width: max-content;
+  display: grid;
+  grid-auto-flow: column;
+  gap: 2.5rem;
+}
+</style>
