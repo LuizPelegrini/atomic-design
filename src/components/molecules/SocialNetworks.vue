57d73c0170e5c454b87de46df64a2118c8b5cf3d
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2021-11-04 15:35:47 +0800

    add: atom SocialLink and molecule SocialNetworks created

---
+++ src/components/molecules/SocialNetworks.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="social-networks">
+    <SocialLink
+      icon="github.svg"
+      name="Github"
+      to="https://github.com/LuizPelegrini"
+    />
+    <SocialLink icon="facebook.svg" name="Facebook" to="https://facebook.com" />
+    <SocialLink icon="twitter.svg" name="Twitter" to="https://twitter.com" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { SocialLink } from '@/components/atoms';
+
+export default defineComponent({
+  components: {
+    SocialLink
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.social-networks {
+  display: grid;
+  grid-gap: 1rem;
+}
+</style>
