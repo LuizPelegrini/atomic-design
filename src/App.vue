f4cf01896c33a9df3aefeadd5806cf156ddcd8d4
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2021-11-04 16:24:40 +0800

    add: organism Services and boson Container created

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,26 @@
 <template>
   <div class="component">
-    <Presentation />
+    <Container>
+      <Header />
+    </Container>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { Presentation } from '@/components/organisms';
+
+import { Container } from '@/components/bosons';
+import { Header } from '@/components/organisms';
 
 export default defineComponent({
   components: {
-    Presentation
+    Header,
+    Container
   }
 });
 </script>
 
 <style lang="scss" scoped>
 .component {
-  max-width: 1200px;
-  width: 100%;
-  margin: 0 auto;
-  padding: 0 1rem;
 }
 </style>
---
+++ src/components/organisms/Services.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="services">
+    <Service
+      title="Frontend"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis optio facere eos, expedita repellat nihil ratione magni enim id officiis voluptatum ab placeat, maiores sit ullam ex architecto et veritatis?"
+    />
+    <Service
+      title="Backend"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis optio facere eos, expedita repellat nihil ratione magni enim id officiis voluptatum ab placeat, maiores sit ullam ex architecto et veritatis?"
+    />
+    <Service
+      title="Maintenance"
+      description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis optio facere eos, expedita repellat nihil ratione magni enim id officiis voluptatum ab placeat, maiores sit ullam ex architecto et veritatis?"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { Service } from '@/components/molecules';
+
+export default defineComponent({
+  components: {
+    Service
+  }
+});
+</script>
+
+<style lang="scss" scoped>
+.services {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 5rem;
+}
+</style>
